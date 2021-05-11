411c83c0a58287b97a20cda2ff6069fd9ce4c85f
Haseeba393 <haseeba393@gmail.com>
2021-05-11 23:16:48 +0500

    add event background

--- app/src/main/java/com/example/eventlogging/fragments/AddEvent.java
+++ app/src/main/java/com/example/eventlogging/fragments/AddEvent.java
@@ -20,23 +20,39 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.eventlogging.Home;
 import com.example.eventlogging.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class AddEvent extends Fragment {
 
     private static final int CAMERA_REQUEST = 1888;
     private static final int MY_CAMERA_PERMISSION_CODE = 100;
 
+    private FirebaseAuth mAuth;
     private ImageView eventCover;
     private Button uploadImgBtn;
     private Button removeImgBtn;
     private TextView eventTitle;
     private TextView eventDescription;
+    private TextView eventLocation;
+    private TextView personName;
+    private TextView personNumber;
     private DatePicker eventDate;
     private Button addEventBtn;
+
+    private ProgressBar addEventLoading;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -49,16 +65,33 @@ public class AddEvent extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
+        // initializing firebase authentication
+        mAuth = FirebaseAuth.getInstance();
+
         eventCover = getActivity().findViewById(R.id.eventCover);
         uploadImgBtn = getActivity().findViewById(R.id.uploadImgBtn);
         removeImgBtn = getActivity().findViewById(R.id.removeImgBtn);
         eventTitle = getActivity().findViewById(R.id.eventTitle);
         eventDescription = getActivity().findViewById(R.id.eventDescription);
+        eventLocation = getActivity().findViewById(R.id.eventLocation);
+        personName = getActivity().findViewById(R.id.personName);
+        personNumber = getActivity().findViewById(R.id.personNumber);
         eventDate = getActivity().findViewById(R.id.eventDate);
         addEventBtn = getActivity().findViewById(R.id.addEventBtn);
+        addEventLoading = getActivity().findViewById(R.id.addEventLoading);
 
         // Setting Minimum Date
         eventDate.setMinDate(System.currentTimeMillis() - 1000);
+
+        // Calling Add new event function on clicking Add EventButton
+        addEventBtn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                _submitNewEvent();
+            }
+        });
 
         // Making click listener for button
         uploadImgBtn.setOnClickListener(new View.OnClickListener()
@@ -76,10 +109,6 @@ public class AddEvent extends Fragment {
             @Override
             public void onClick(View v)
             {
-                // Setting Upload icon to imageview
-                Drawable drawable = getResources().getDrawable(R.drawable.upload);
-                eventCover.setImageDrawable(drawable);
-
                 // Change visiblility of remove button to gone
                 removeImgBtn.setVisibility(View.GONE);
 
@@ -140,5 +169,98 @@ public class AddEvent extends Fragment {
         catch (Exception e){
             Toast.makeText(getActivity(), e.getMessage(), Toast.LENGTH_LONG).show();
         }
+    }
+
+    // Submitting new event to realtime database
+    public void _submitNewEvent () {
+
+        //Validations
+
+        try {
+            if(eventCover.getDrawable() == null) {
+                Toast.makeText(getActivity(), "Event Cover is required", Toast.LENGTH_LONG).show();
+            }
+            else if(eventTitle.getText().toString().length() == 0){
+                Toast.makeText(getActivity(), "Event Title is required", Toast.LENGTH_LONG).show();
+            }
+            else if(eventDescription.getText().toString().length() == 0){
+                Toast.makeText(getActivity(), "Event Description is required", Toast.LENGTH_LONG).show();
+            }
+            else if(eventLocation.getText().toString().length() == 0){
+                Toast.makeText(getActivity(), "Event Location is required", Toast.LENGTH_LONG).show();
+            }
+            else if(personName.getText().toString().length() == 0){
+                Toast.makeText(getActivity(), "Person Name is required", Toast.LENGTH_LONG).show();
+            }
+            else if(personNumber.getText().toString().length() == 0){
+                Toast.makeText(getActivity(), "Person Number is required", Toast.LENGTH_LONG).show();
+            }
+            else {
+
+                // Show Loading
+                addEventLoading.setVisibility(View.VISIBLE);
+
+                // Creating Database reference to add event details
+                FirebaseUser currentUser = mAuth.getCurrentUser();
+                String uid = currentUser.getUid();
+
+                // Wrinting extra information of user in database
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference myRef = database.getReference("events");
+
+                // creating date string format .e.g. 05-06-1996
+                String date = String.valueOf(eventDate.getDayOfMonth()) + "-" + String.valueOf(eventDate.getDayOfMonth()) + "-" + String.valueOf(eventDate.getDayOfMonth());
+
+                // Creating Key Value pair for realtime database using hashmap
+                HashMap<String , Object> databaseMap = new HashMap<>();
+                databaseMap.put("event_title",eventTitle.getText().toString());
+                databaseMap.put("event_description",eventDescription.getText().toString());
+                databaseMap.put("event_location",eventLocation.getText().toString());
+                databaseMap.put("event_date",date);
+                databaseMap.put("userUID",uid);
+                databaseMap.put("event_cover","");
+
+                String pushKey = myRef.push().getKey();
+
+                myRef.child(pushKey).setValue(databaseMap)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+
+                        // Clear the fields
+                        eventCover.setImageDrawable(null);
+                        eventTitle.setText("");
+                        eventDescription.setText("");
+                        eventLocation.setText("");
+                        personName.setText("");
+                        personNumber.setText("");
+
+
+                        Toast.makeText(getActivity(),"Event is added successfully",Toast.LENGTH_LONG).show();
+                        addEventLoading.setVisibility(View.GONE);
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getActivity(),e.getMessage(),Toast.LENGTH_LONG).show();
+                        addEventLoading.setVisibility(View.GONE);
+                    }
+                });
+            }
+        }
+        catch (Exception e){
+            Toast.makeText(getActivity(), e.toString(), Toast.LENGTH_LONG).show();
+        }
+
+
+//        if(eventCover == null){
+//            Toast.makeText(getActivity(), "Event cover is required", Toast.LENGTH_LONG).show();
+//        }
+//        else{
+//            Toast.makeText(getActivity(), "nextv alidation", Toast.LENGTH_LONG).show();
+//        }
+
     }
 }
